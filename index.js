65d043a39c65de87cd0e9bc20b54cd4204da2610
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-04-21 10:24:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
+const port = 3000
+const mensagens =[
+  "essa é a primeira mensagem",
+  "essa é a segunda mensagem"
+];
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
-app.listen(3000, function(){
-    console.log("app rodando em http://localhost:3000")
+app.get('/mensagens', (req, res) =>{
+  res.send(mensagens);
+});
+
+app.get('/mensagens/:id', (req, res) =>{
+
+  const id = req.params.id;
+  const mensagem = mensagens[id];
+  
+  res.send(mensagem);
+});
+
+app.listen(port, function(){
+  console.log(`app rodando em http://localhost:${port}`)
 })
